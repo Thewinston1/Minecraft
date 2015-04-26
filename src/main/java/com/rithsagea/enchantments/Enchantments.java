772b4ffc556959c4aa1794ba7344421c2733aa97
winston <thestarwinston@gmail.com>
2015-04-26 10:38:52 -0400

    .

---
+++ src/main/java/com/rithsagea/enchantments/Enchantments.java
@@ -0,0 +1,12 @@
+package com.rithsagea.enchantments;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid=Enchantments.MODID, version = Enchantments.VERSION)
+public class Enchantments{
+
+	public static final String MODID="Enchantments";
+	public static final String VERSION="1.0";
+}
